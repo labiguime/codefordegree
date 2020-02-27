a9b7e328c3e76bf376eadef158ec8f9303fee280
Kakashi <rpranta@sfu.ca>
2020-02-27 02:49:53 -0800

    "Formatting the code using prettier"

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,38 @@
 "use-strict";
 
 /* Environment Variables from .env */
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 /* Installed Modules */
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-
-
+const mongoose = require("mongoose");
 
 /* Setup database */
-mongoDBUrl='mongodb://localhost/code4degree';
-mongoose.connect(mongoDBUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoDBUrl = "mongodb://localhost/code4degree";
+mongoose.connect(mongoDBUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open',function(){ console.log('Database connected'); });
-
-
-
-/* Set up View */ 
-app.set('views', './views'); 	//Can used for quickly testing out the backend
-app.set('view engine', 'pug');
-
-
-
-/* Routes */
-app.get('/', (req, res) => {
-	res.render('welcome', {title: "Code4Degree", message: "An online assignment submission platform"});
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function() {
+  console.log("Database connected");
 });
 
+/* Set up View */
+app.set("views", "./views"); 
+app.set("view engine", "pug"); //Can used for quickly testing out the backend
 
+/* Routes */
+app.get("/", (req, res) => {
+  res.render("welcome", {
+    title: "Code4Degree",
+    message: "An online assignment submission platform"
+  });
+});
 
 app.listen(port, () => console.log(`${env} server listening on port ${port}!`));
