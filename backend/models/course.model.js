5caa59a7ebd1580b77aa78fa9b36e274eb1925bb
tln3 <tln3@sfu.ca>
2020-02-29 12:49:56 -0800

    Added course schema

---
+++ backend/models/course.model.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var courseSchema = new Schema({
+    name: String,
+    description: String,
+    term: {
+        type: String,
+        enum: ["Spring", "Summer", "Fall"],
+        required: [true, "Term is needed to distinguish courses"]   
+    },
+    admin_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+var Course = mongoose.Model('Course', courseSchema);
+module.exports = Course;
+
