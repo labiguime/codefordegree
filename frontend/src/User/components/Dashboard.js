ce905453fbf8a344a22314a50549e1965e63b65d
tln3 <tln3@sfu.ca>
2020-03-11 12:19:11 -0700

    Added header for course group

--- frontend/src/User/components/Dashboard.js
+++ frontend/src/User/components/Dashboard.js
@@ -16,8 +16,14 @@ import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import { AuthContext } from '../../shared/context/auth-context';
 import Header from './Header';
-
-
+import SideBar from '../../shared/components/SideBar';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import Divider from '@material-ui/core/Divider';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import ToolTip from '@material-ui/core/Tooltip';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -69,6 +75,12 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
+  iconAlignRight: {
+      marginLeft:'auto' 
+  },
+  courseGroupHeader: {
+    marginBottom: 20 
+  }
 }));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -77,82 +89,64 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 export default function Dashboard(props){
   const classes = useStyles();
 
+  const content = (
+    <main>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <div className={classes.courseGroup}  >
+            <div className={classes.courseGroupHeader} style={{display: "flex"}} >
+              <Typography variant="h4" > 
+                Administrative Courses
+              </Typography>
+              <ToolTip title="Create course" placement="top">
+                <IconButton color="primary" className={classes.iconAlignRight} onClick={() => {}}>
+                    <AddIcon />
+                </IconButton>
+              </ToolTip>
+              <Divider />
+            </div>
+            <Grid container spacing={4}>
+              {cards.map(card => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        CMPT 470 - Web-based information system 
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to describe the content.
+                        alalallalalalalallalala
+                      </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing >
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <section className={classes.iconAlignRight}>
+                          <IconButton size="small" color="primary" onClick={() => console.log("edit")}>
+                              <EditIcon/> 
+                          </IconButton>
+                          <IconButton  size="small" color="primary" onClick={() => console.log("delete")}>
+                              <DeleteIcon /> 
+                          </IconButton>
+
+                      </section>
+
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          
+        </Container>
+      </main>
+  )
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <Header title="Code4Degree" sections={sections} />
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Album layout
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
-        <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map(card => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
+      <SideBar title={"Courses"} content={content} />
+      
     </React.Fragment>
   );
 }
