6a5f72c402a46d9ce251bbe4ac9c94c6471ec586
labiguime <leoabiguime@gmail.com>
2020-03-31 18:14:06 -0700

    Fix EnrollmentModal UI design

--- frontend/src/shared/components/EnrollmentModal.js
+++ frontend/src/shared/components/EnrollmentModal.js
@@ -7,6 +7,9 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 
 const useStyles = makeStyles(theme => ({
@@ -18,16 +21,39 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(1),
     width: "40%",
   },
+  cardGrid: {
+	paddingTop: theme.spacing(8),
+	paddingBottom: theme.spacing(8),
+  },
+  card: {
+	height: '100%',
+	display: 'flex',
+	flexDirection: 'column',
+  },
+  cardMedia: {
+	paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+	flexGrow: 4,
+  },
   formControl: {
     margin: theme.spacing(1),
     minWidth: "30%",
+  },
+  searchFieldAppearance: {
+      minWidth: "40%",
+	  textAlign: "center",
+  },
+  selectFieldAppearance: {
+	  margin: theme.spacing(1),
+      minWidth: "40%",
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    float: "right",
+    float: "left",
   },
   boxField: {
     marginLeft: theme.spacing(1),
@@ -41,7 +67,9 @@ export default function EnrollmentModal(props) {
     const defaultValueMap = props.defaultValueMap || {};
     const [courseList, setCourseList] = React.useState(defaultValueMap);
 	const [temporaryList, setTemporaryList] = React.useState(defaultValueMap);
+	const [chosenCourse, setChosenCourse] = React.useState();
 	console.log(courseList);
+	console.log(temporaryList[1].name);
     const handleListChange = (e) => {
 		let currentList = {};
 		let newList = {};
@@ -53,20 +81,52 @@ export default function EnrollmentModal(props) {
 		});
 		setTemporaryList(newList);
 	}
+	const handleCourseChanged = (e) => {
+		setChosenCourse(e.target.value);
+	}
 	return (
 		<div>
-		<div>
-		    <input type="text" className="input" placeholder="Search..." onChange={handleListChange} />
-		</div>
-
-		<ul>
-		  {temporaryList.map(item => (
-		    <li key={item._id}>
-		      {item.name} &nbsp;
-		    </li>
-		  ))}
-		</ul>
-
+		<Grid container justify = "center">
+			<Grid item xs={12}>
+					<Grid container justify = "center">
+					<TextField
+					  label="Search..."
+					  color='secondary'
+					  className={classes.searchFieldAppearance}
+					  onChange={handleListChange}
+					/>
+					</Grid>
+					<br />
+					<Grid container justify = "center">
+					<FormControl className={classes.selectFieldAppearance}>
+			          <Select
+					  	id="Chosen"
+			            color='secondary'
+						defaultValue={temporaryList[0].name}
+						onChange={handleCourseChanged}
+			          >
+					  {temporaryList.map(item => (
+						<MenuItem value={item._id}>{item.name}</MenuItem>
+					  ))}
+			          </Select>
+			        </FormControl>
+					</Grid>
+					<br />
+					<br />
+					<Grid container justify = "center">
+					<Button
+						type="submit"
+						size="medium"
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+						onClick={() => props.onSubmit(chosenCourse)}
+					>
+					  {props.buttonTitle}
+					</Button>
+					</Grid>
+			</Grid>
+		  </Grid>
 		</div>
 	);
 }
