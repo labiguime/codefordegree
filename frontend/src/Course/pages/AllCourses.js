a98a662e865586bc085f6ebbd50c16a83777c0f3
tln3 <tln3@sfu.ca>
2020-04-01 11:54:26 -0700

    HOTFIX: Removed duplicated package and added link for viewing enrolled course

--- frontend/src/Course/pages/AllCourses.js
+++ frontend/src/Course/pages/AllCourses.js
@@ -330,8 +330,9 @@ export default function AllCourses(props){
                         </Typography>
                       </CardContent>
                       <CardActions disableSpacing >
-                        <Button size="small" color="primary">
-                          View
+                        <Button size="small" color="primary"
+                        >
+                          <Link className={classes.linkStyle} to={ "/course/"+_id}>View</Link>
                         </Button>
                             <section className={classes.iconAlignRight}>
                                 <IconButton
@@ -389,7 +390,9 @@ export default function AllCourses(props){
                         </Typography>
                       </CardContent>
                       <CardActions disableSpacing >
-                        <Button size="small" color="primary" color="inherit"><Link className={classes.linkStyle} to={ "/course/"+allCourses[idx]._id}>View</Link></Button>
+                        <Button size="small" color="primary" color="inherit">
+                          <Link className={classes.linkStyle} to={ "/course/"+allCourses[idx]._id}>View</Link>
+                        </Button>
                         {admin_id == auth.userInfo._id &&
                             <section className={classes.iconAlignRight}>
                                 <IconButton size="small"
